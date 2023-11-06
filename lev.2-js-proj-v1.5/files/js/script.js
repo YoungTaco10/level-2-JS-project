dfc0a74f4538dc2d127125572f29a42814465918
ICanHandleSpice <146298943+ICanHandleSpice@users.noreply.github.com>
2023-11-06 11:01:17 -0500

    Add files via upload

---
+++ lev.2-js-proj-v1.5/files/js/script.js
@@ -0,0 +1,15 @@
+
+
+
+class Location{
+
+    constructor(name, text, buttons, locations, isSpecial ){
+        this.name = name;
+        this.text = text;
+        this. buttons = buttons;
+        this.locations = locations;
+        this.isSpecial = isSpecial;
+    }
+    numButtons = this.locations.length;
+
+}
